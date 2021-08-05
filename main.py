9507276fbb4864e034bcce92952b85034ebc0a0a
Amin <aminoffline@yahoo.com>
2021-08-06 01:48:27 +0430

    It's Just work

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
-# This is a sample Python script.
+# movie genres
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+#movie_genres = {'Horror':0, 'Romance':0, 'Comedy':0, 'History':0 , 'Adventure':0 , 'Action':0}
+
+def INPUT(n):
+    movie_genres = {'Horror': 0, 'Romance': 0, 'Comedy': 0, 'History': 0, 'Adventure': 0, 'Action': 0}
+    for i in range(0,n):
+        name,choice1,choice2,choice3 =input().split(sep=" ")
+        if choice1 in movie_genres.keys():
+            movie_genres[f'{choice1}'] += 1
+        if choice2 in movie_genres.keys():
+            movie_genres[f'{choice2}'] += 1
+        if choice3 in movie_genres.keys():
+            movie_genres[f'{choice3}'] += 1
+    return movie_genres
+repetition = int(input())
+A = INPUT(repetition)
+A = dict(sorted(A.items(),key=lambda item : (-item[1],item[0])))
+
+for k , v in A.items():
+    print(f'{k}:{v}', end='\n')
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
